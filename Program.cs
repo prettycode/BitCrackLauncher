02fef24ae2c2dc4d0f7abf017a1bed50c2f55227
Chris <github@prettycode.org>
2024-12-27 21:51:29 -0800

    Previous puzzle solved. Update to new puzzle.

--- Program.cs
+++ Program.cs
@@ -13,13 +13,13 @@ namespace BitCrackLauncher
 {
     internal class ProgramConfig
     {
-        public BitcoinPuzzle Puzzle { get; set; } = new("13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so", "20000000000000000", "3ffffffffffffffff");
+        public BitcoinPuzzle Puzzle { get; set; } = new("1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9", "40000000000000000", "7ffffffffffffffff");
 
         public TimeSpan TimeBeforeRestart { get; set; } = TimeSpan.Parse("08:00:00");
 
         public int ConcurrentCrackerInstancesCount { get; set; } = 5;
 
-        public string CrackerPath { get; set; } = @"C:\Users\chris\Documents\GitHub\BitCrackLauncher\cuBitCrack.exe";
+        public string CrackerPath { get; set; } = @"C:\GitHub\BitCrackLauncher\clBitCrack.exe";
 
         public string SolutionOutputFilePattern { get { return Path.Combine(Path.GetDirectoryName(this.CrackerPath), "solution.*.txt"); } }
 
